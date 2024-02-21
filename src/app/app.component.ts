76dd5e0a0d7419b09c7817368e4b64fc530a48d3
Bastien <record.bastien@gmail.com>
2024-02-21 18:55:12 +0100

    new(Menu)
    Create navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from "./menu/menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, MenuComponent]
 })
+
 export class AppComponent {
   title = 'bastos-rcd.github.io';
 }
